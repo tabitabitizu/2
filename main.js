f9a3ac60e613e40698342887881c41d3e84ad065
tabitabitizu <panboleslav@gmail.com>
2018-04-08 23:31:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -31,3 +31,19 @@ $(window).scroll(function() {
     });
   });
 })
+
+$(".carousel").on("touchstart", function(event){
+        var xClick = event.originalEvent.touches[0].pageX;
+    $(this).one("touchmove", function(event){
+        var xMove = event.originalEvent.touches[0].pageX;
+        if( Math.floor(xClick - xMove) > 5 ){
+            $(this).carousel('next');
+        }
+        else if( Math.floor(xClick - xMove) < -5 ){
+            $(this).carousel('prev');
+        }
+    });
+    $(".carousel").on("touchend", function(){
+            $(this).off("touchmove");
+    });
+});
